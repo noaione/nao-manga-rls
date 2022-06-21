ea9fbc0b1cf6352cbc4df1dd7a1c2822a47c21df
noaione <noaione0809@gmail.com>
2022-06-21 12:59:57 +0800

    cmd: actually add spreads cmd

--- nmanga/cmd.py
+++ nmanga/cmd.py
@@ -30,6 +30,7 @@ import click
 
 from .cli.auto_split import auto_split
 from .cli.color_level import color_level
+from .cli.join_spreads import spreads_join
 from .constants import __author__, __name__, __version__
 from .term import get_console
 
@@ -58,6 +59,7 @@ def main(ctx: click.Context):
 
 main.add_command(auto_split)
 main.add_command(color_level)
+main.add_command(spreads_join)
 
 
 if __name__ == "__main__":
