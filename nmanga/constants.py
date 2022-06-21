446f8e27195bb2683e5cb08111aa887ab723fc90
noaione <noaione0809@gmail.com>
2022-06-21 08:28:19 +0800

    Bootstrap project

---
+++ nmanga/constants.py
@@ -0,0 +1,8 @@
+__name__ = "nmanga"
+__author__ = "noaione"
+__author_email__ = "noaione@n4o.xyz"
+__version__ = "0.1.0"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2022-present noaione"
+
+__all__ = ("__name__", "__author__", "__version__", "__license__", "__copyright__")
