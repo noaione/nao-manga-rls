b1b056330024656c82f13721fa1d6edd3f8b0978
noaione <noaione0809@gmail.com>
2023-10-30 16:16:19 +0700

    v0.12.0

--- setup.py
+++ setup.py
@@ -65,8 +65,6 @@ setup_args = dict(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
@@ -81,7 +79,7 @@ setup_args = dict(
         "Source": "https://github.com/noaione/nao-manga-rls",
     },
     entry_points={"console_scripts": ["nmanga=nmanga.cmd:main"]},
-    python_requires=">=3.7",
+    python_requires=">=3.9",
 )
 
 setup(**setup_args)
